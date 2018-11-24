8b985977303829b5520fae05216aa221887d8ff5
tarakanov <tarakanov.sv@gmail.com>
2018-11-24 23:27:12 +0500

    one more commit

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World! "
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0',port=8080)
